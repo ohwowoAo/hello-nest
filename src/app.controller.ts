56c81f7ef1a6617cba294e14f8aad97626f13524
EunJeong <sbjdnm@gmail.com>
2025-07-29 11:47:11 +0900

    feat : 기본구조 파악 및 컨트롤러 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //express의 get 라우터같은 역할
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello')
+  sayHello(): string {
+    return 'Hello, everyone';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//데코레이터
 @Module({
   imports: [],
+  //express의 라우터같은 역할 - url을 가져오고 함수를 실행
   controllers: [AppController],
   providers: [AppService],
 })
