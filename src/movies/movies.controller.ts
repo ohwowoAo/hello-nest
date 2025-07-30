a012dad91fffc85fc61686222876f8c334166b0b
EunJeong <sbjdnm@gmail.com>
2025-07-30 16:31:45 +0900

    feat : 무비 update 구현

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -32,9 +32,6 @@ export class MoviesController {
 
     @Patch(':id')
     patch(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updateMovie: movieId,
-            ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
     }
 }
