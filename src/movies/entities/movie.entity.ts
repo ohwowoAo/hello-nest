7f545e107171b23f69ca08eea499a161ebb56624
EunJeong <sbjdnm@gmail.com>
2025-07-31 12:06:41 +0900

    feat : update dto 작업

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,6 +1,6 @@
 export class Movie {
     id: number;
-    title: string;
-    year: number;
-    genres: string[];
+    title?: string;
+    year?: number;
+    genres?: string[];
 }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -32,7 +33,7 @@ export class MoviesController {
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId: number, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updateData);
     }
 }
