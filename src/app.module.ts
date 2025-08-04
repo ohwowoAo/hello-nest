34b3cd8689f15c232c12e96665cad8ff9dfb403b
EunJeong <sbjdnm@gmail.com>
2025-08-04 10:57:14 +0900

    feat : movies module 생성하여 module 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
 
 //데코레이터
 @Module({
-    imports: [],
+    imports: [MoviesModule],
     //express의 라우터같은 역할 - url을 가져오고 함수를 실행
-    controllers: [AppController, MoviesController],
-    providers: [AppService, MoviesService],
+    controllers: [AppController],
+    providers: [],
 })
 export class AppModule {}
