e7d89700d4d651ce70cd5f91982708859243f258
EunJeong <sbjdnm@gmail.com>
2025-08-05 11:53:00 +0900

    test: getAll, getOne, deleteOne, create 메서드 테스트 코드

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
     let service: MoviesService;
@@ -14,5 +15,54 @@ describe('MoviesService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('getAll', () => {
+        it('should return an array', () => {
+            const result = service.getAll();
+            expect(result).toBeInstanceOf(Array);
+        });
+    });
+
+    describe('getOne', () => {
+        it('should return a movie', () => {
+            service.create({ title: 'Test Movie', year: 2021, genres: ['Drama'] });
+            const movie = service.getOne(1);
+            expect(movie).toBeDefined();
+            expect(movie.id).toEqual(1);
+        });
+        it('should throw 404 error', () => {
+            try {
+                service.getOne(999);
+            } catch (e) {
+                expect(e).toBeInstanceOf(NotFoundException);
+            }
+        });
+    });
+    describe('deleteOne', () => {
+        it('deletes a movie', () => {
+            service.create({ title: 'Test Movie', year: 2021, genres: ['Drama'] });
+            const beforeDelete = service.getAll().length;
+            service.deleteOne(1);
+            const afterDelete = service.getAll().length;
+            expect(afterDelete).toBeLessThan(beforeDelete);
+        });
+        it('should throw 404 error', () => {
+            try {
+                service.deleteOne(999);
+            } catch (e) {
+                expect(e).toBeInstanceOf(NotFoundException);
+            }
+        });
+    });
+
+    describe('create', () => {
+        it('should create a movie', () => {
+            const beforeCreate = service.getAll().length;
+            service.create({ title: 'New Movie', year: 2022, genres: ['Action'] });
+            const afterCreate = service.getAll().length;
+            console.log(beforeCreate, afterCreate);
+            expect(afterCreate).toBeGreaterThan(beforeCreate);
+        });
     });
 });
